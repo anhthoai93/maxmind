7f080c0a45c11238bf6b4eac73e0ce7ee98867fa
Andy <thoaitran93@gmail.com>
2024-04-24 01:00:37 +0700

    Store data in SQL

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
 from flask import Flask
 from flask_smorest import Api
+from db import db
 from resources.store import blp as StoreBlueprint
 from resources.item import blp as itemBlueprint
 
 
-app = Flask(__name__)
+def create_app(db_url=None):
+    app = Flask(__name__)
 
-app.config["PROPAGATE_EXCEPTIONS"] = True
-app.config["API_TITLE"] = "Stores REST API"
-app.config["API_VERSION"] = "v1"
-app.config["OPENAPI_VERSION"] = "3.0.3"
-app.config["OPENAPI_URL_PREFIX"] = "/"
-app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///data.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
 
-api = Api(app)
+    api = Api(app)
+    with app.app_context():
+        db.create_all()
 
-api.register_blueprint(StoreBlueprint)
-api.register_blueprint(itemBlueprint)
+    api.register_blueprint(StoreBlueprint)
+    api.register_blueprint(itemBlueprint)
+    return app
