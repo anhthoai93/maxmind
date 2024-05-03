8121658f7e68df3b949973cef6a825dbaaf00fd6
Andy <thoaitran93@gmail.com>
2024-05-04 01:04:34 +0700

    Add mailgun and redis

--- resources/user.py
+++ resources/user.py
@@ -1,10 +1,7 @@
+import os
+
+import redis
 from flask.views import MethodView
-from flask_smorest import Blueprint, abort
-from db import db
-from models import UserModel
-from schemas import UserSchema
-from blocklist import BLOCKLIST
-from passlib.hash import pbkdf2_sha256
 from flask_jwt_extended import (
     jwt_required,
     get_jwt_identity,
@@ -12,23 +9,41 @@ from flask_jwt_extended import (
     create_refresh_token,
     get_jwt
 )
+from flask_smorest import Blueprint, abort
+from passlib.hash import pbkdf2_sha256
+from rq import Queue
+from sqlalchemy import or_
+
+from blocklist import BLOCKLIST
+from db import db
+from models import UserModel
+from schemas import UserSchema, UserRegisterSchema
+from tasks import send_user_registration_email
 
 blp = Blueprint("Users", "users", description="Operations on users")
 
 
+connection = redis.from_url(os.environ.get("REDIS_URL"))
+queue = Queue("emails", connection=connection)
 @blp.route("/register")
 class UserRegister(MethodView):
 
-    @blp.arguments(UserSchema)
+    @blp.arguments(UserRegisterSchema)
     def post(self, user_data):
-        if UserModel.query.filter_by(username=user_data["username"]).first():
+        print(user_data)
+        if UserModel.query.filter(or_(
+                UserModel.username == user_data["username"],
+                UserModel.email == user_data["email"])
+        ).first():
             abort(409, message="A user with that username already exists.")
         user = UserModel(
             username=user_data["username"],
             password=pbkdf2_sha256.hash(user_data["password"]),
+            email=user_data["email"],
         )
         db.session.add(user)
         db.session.commit()
+        queue.enqueue(send_user_registration_email, user.email, user.username)
         return {"message": "User created successfully."}, 201
 
 
--- schemas.py
+++ schemas.py
@@ -26,6 +26,7 @@ class TagSchema(PlainTagSchema):
     store_id = fields.Int(load_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
 
+
 class ItemSchema(PlainItemSchema):
     store_id = fields.Int(required=True, load_from=True)
     stores = fields.Nested(PlainStoreSchema(), dump_only=True)
@@ -46,3 +47,7 @@ class UserSchema(Schema):
     id = fields.Int(dump_only=True)
     username = fields.Str(required=True)
     password = fields.Str(required=True, load_only=True)
+
+
+class UserRegisterSchema(UserSchema):
+    email = fields.Str(required=True)
